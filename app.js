52541f6ba49321361a0cc84228758177767d1966
Anirban Majumder <81350357+anirbanmajumder0@users.noreply.github.com>
2023-12-23 23:56:34 +0530

    Add dotenv and mongoose dependencies, update MongoDB connection, and implement Serviceability model and API

--- app.js
+++ app.js
@@ -1,22 +1,37 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
 const express = require('express');
 const path = require('path');
 const app = express();
 
-const PORT = process.env.PORT || 3000;
 
+const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => console.log('MongoDB connected...'))
+  .catch(err => console.log(err));
+// Define a function to fetch and log all documents from a collection
 
-const mrchntId = ["10001","100002","10003"];
-const pincodes = ["700015","70001","700020","70023"];
+const ServiceabilitySchema = new mongoose.Schema({
+  _id: Number,
+  mID: [String]
+});
+
+const Serviceability = mongoose.model('Serviceability', ServiceabilitySchema);
+
 app.post('/search', (req, res) => {
-    const pin = req.body.pin;
-    if(pincodes.includes(pin)) {
-        res.json({ merchantIds: mrchntId });
-    } else {
+  const pin = req.body.pin;
+  Serviceability.findById(pin)
+    .then(doc => {
+      if (doc) {
+        res.json({ merchantIds: doc.mID });
+      } else {
         res.json({ merchantIds: [] });
-    }
+      }
+    })
+    .catch(err => console.log(err));
 });
 
 app.get('/', (req, res) => {
