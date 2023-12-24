f9b03c92495597b79e39e680993e3deac03b68dd
Anirban Majumder <81350357+anirbanmajumder0@users.noreply.github.com>
2023-12-24 18:18:26 +0530

    dubuging

--- app.js
+++ app.js
@@ -22,8 +22,11 @@ const ServiceabilitySchema = new mongoose.Schema({
 
 // Create a Mongoose model from the schema
 const Serviceability = mongoose.model('Serviceability', ServiceabilitySchema);
+//list all the documents in the collection that has mid lenght more than 0
 
-
+Serviceability.find({ mID: { $exists: true, $not: { $size: 0 } } })
+  .then(docs => console.log(docs))
+  .catch(err => console.log(err));
 // Define a POST route for '/search'
 app.post('/search', (req, res) => {
   const pin = req.body.pin; // Get the pin code from the request body
@@ -54,9 +57,39 @@ app.get('/admin', (req, res) => {
 
 app.post('/upload', (req, res) => {
   const data = req.body;
-  console.log(data);
-  res.json({ message: 'Data received' });
-});
+  console.log('Received data:', data); // Log the received data
 
+  // Get all the merchant IDs from the data
+  const mIDs = Object.keys(data);
+  console.log('Merchant IDs:', mIDs); // Log the merchant IDs
+
+  // For each merchant ID, process the associated pincodes
+  Promise.all(mIDs.map(mID => {
+    const pincodes = data[mID];
+    console.log(`Processing pincodes for merchant ID ${mID}:`, pincodes); // Log the pincodes for each merchant ID
+
+    return Promise.all(pincodes.map(pincode => {
+      console.log(`Updating document for pincode ${pincode} and merchant ID ${mID}`); // Log the pincode and merchant ID being processed
+
+      // For each pincode, find the associated document and update it, or create a new one if it doesn't exist
+      return Serviceability.updateOne(
+        { _id: pincode },
+        { $addToSet: { mIDs: 'M000000004' } },
+        { upsert: true }
+      )
+      //.then(res => console.log('Response from server:', res))
+      .catch(err => console.log('Error updating document:', err)); 
+    }));
+  }))
+  .then(() => {
+    console.log('Data received and stored', mIDs);
+    res.json({ message: 'Data received and stored' });
+  })
+  .catch(err => {
+    console.log('Error processing data:', err); // Log any errors that occur during processing
+    res.status(500).json({ message: 'An error occurred' });
+  });
+});
+ 
 // Start the server
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
--- public/admin.js
+++ public/admin.js
@@ -1,56 +1,54 @@
-function parseRow(row, pincode) {
-  var result = {};
-  for (var i = 1; i < row.data.length; i++) {
-    var data = row.data[i];
-    if (data !== 'm' && data !== 0) {
-      if (!result[row.data[0]]) {
-        result[row.data[0]] = [];
-      }
-      result[row.data[0]].push(pincode[i]);
+function processRow(row, pincode) {
+    var result = {};
+    for (var i = 1; i < row.data.length; i++) {
+        var data = row.data[i];
+        if (data !== 'm' && data !== 0) {
+            if (!result[row.data[0]]) {
+                result[row.data[0]] = [];
+            }
+            result[row.data[0]].push(pincode[i]);
+        }
     }
-  }
-
-  // Upload the result to the server
-  fetch('/upload', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(result)
-  })
-  .then(response => response.json())
-  //.then(data => console.log(data))
-  .catch((error) => {
-    console.error('Error:', error);
-  });
-
-  return result;
+    return result;
 }
 
-function parseCsv() {
-  var i = 0;
-  var pincode = [];
-  var file = document.getElementById('csv-file').files[0];
-  var size = file.size;
-  var percent = 0;
+// Function to upload data to the server
+function uploadData(data) {
+    return fetch('/upload', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+    .then(response => response.json())
+    .catch((error) => {
+        console.error('Error:', error);
+    });
+}
 
-  Papa.parse(file, {
-    worker: true,
-    dynamicTyping: true,
-    step: function(row) {
-      if (i === 0) {
-        pincode = row.data;
-        i++;
-      } else {
-        parseRow(row, pincode);
-      }
-      var progress = row.meta.cursor;
-      console.log(progress);
-      percent = Math.round(progress / size * 100);
-      document.getElementById('loading-bar').style.width = percent + '%';
-    },
-    complete: function() {
-      console.log("All done!");
-    }
-  });
+// Function to parse CSV file
+function parseCsv() {
+    const file = document.getElementById('csv-file').files[0];
+    const fileSize = file.size;
+    let headers = [];
+
+    Papa.parse(file, {
+        worker: true,
+        dynamicTyping: true,
+        step: function(row) {
+            const percent = Math.round(row.meta.cursor/ fileSize * 100);
+            document.getElementById('loading-bar').style.width = percent + '%';
+
+            if (headers.length === 0) {
+                headers = row.data;
+            } else {
+                const rowData = processRow(row, headers);
+                uploadData(rowData);
+            }
+        },
+        complete: function() {
+            console.log("All done!");
+        }
+    });
 }
